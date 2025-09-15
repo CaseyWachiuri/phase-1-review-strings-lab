1c946e77e404893e8235c64f5ea1317c6aa6ab63
Casey Waweru <casey.waweru@gmail.com>
2025-09-15 08:31:19 +0300

    Recap on Strings in JS

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Grace Hoper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
